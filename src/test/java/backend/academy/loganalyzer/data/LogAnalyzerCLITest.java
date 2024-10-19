b773eb172cfa50b730f9f83fe290d58fffa1723d
Dmitriy <golenko_dmitriy@mail.ru>
2024-10-20 05:21:02 +0700

    fix: исправлено различие в порядке

--- src/test/java/backend/academy/loganalyzer/data/LogAnalyzerCLITest.java
+++ src/test/java/backend/academy/loganalyzer/data/LogAnalyzerCLITest.java
@@ -1,6 +1,7 @@
 package backend.academy.loganalyzer.data;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertIterableEquals;
 import backend.academy.loganalyzer.cli.LogAnalyzerCLI;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
@@ -25,11 +26,17 @@ class LogAnalyzerCLITest {
             "agent:Debian",
             "--filter",
             "method:GET"};
-        List<String> paths = List.of("src/test/resources/logs/logs1/2024-08-31.txt", "src/test/resources/logs/logs2/2024-08-31.txt", "src/test/resources/logs/logs3/2024-08-31.txt");
+        List<String> paths = new java.util.ArrayList<>(
+            List.of("src/test/resources/logs/logs1/2024-08-31.txt", "src/test/resources/logs/logs2/2024-08-31.txt",
+                "src/test/resources/logs/logs3/2024-08-31.txt"));
 
         var result = logAnalyzer.parse(args);
 
-        assertEquals(paths, result.paths());
+        var resultPath = result.paths();
+        paths.sort(String::compareTo);
+        resultPath.sort(String::compareTo);
+
+        assertEquals(paths, resultPath);
         assertEquals(LocalDate.parse("2015-05-18", DateTimeFormatter.ISO_DATE).atStartOfDay(), result.to());
         assertEquals(LocalDate.parse("2015-05-17", DateTimeFormatter.ISO_DATE).atStartOfDay(), result.from());
         assertEquals("adoc", result.format());
